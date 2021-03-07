f2c6ece1f19d40945609ac40519660fac2b4b594
gurelbs <guri240@gmail.com>
2021-03-07 16:41:31 +0200

    add readme file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ cardNum = cardNum.sort(() => 0.5 - Math.random())
 const $ = x => document.querySelector(x)
 const game = $('.game')
 const cards = $('.cards')
+const score = $('.score')
 const allCard = document.querySelectorAll('.card')
 
 
@@ -19,23 +20,38 @@ const createCard = () => {
 }
 createCard()
 
-const state = {}
-state.numbers = []
-state.numOfClick = 0
+const state = {
+    numbers: [],
+    numOfClick: 0,
+    score: 0,
+    isCurrect: false
+}
 
 const handleClicks = e => {
-    if (state.numOfClick < 2) {
-        if (!e.target.classList.contains('opacity') && state.numOfClick < 2) {
-            e.target.classList.add('opacity')
-            state.numOfClick++
-                state.numbers.push(e.target.textContent)
+    let cardsChildren = [...cards.children]
+    console.log(cardsChildren);
+    if (e.target.classList.contains('card') && state.numOfClick < 2) {
+        state.numbers.push(e.target.textContent);
+        state.numOfClick++;
+        e.target.classList.add('opacity');
+        e.target.classList.remove('color-transparent')
+        const showNumber = () => {
+            e.target.classList.remove('opacity');
+            e.target.classList.add('color-transparent')
         }
+        setTimeout(showNumber, 1000)
+    }
+    state.numbers[0] === state.numbers[1] ? state.isCurrect = true : state.isCurrect
+    if (state.isCurrect === true) {
+        console.log(state.numbers[0], state.numbers[1]);
+        state.score += 10
+        score.textContent = state.score
+        state.numbers = []
+        state.numOfClick = 0
+    } else {
         console.log(state.numbers);
-        if (state.numbers[0] === state.numbers[1]) {
-            console.log('currect guess !');
-        } else {
-            console.log('wrong guess');
-        }
+        state.numbers = []
+        state.numOfClick = 0
     }
 }
 
